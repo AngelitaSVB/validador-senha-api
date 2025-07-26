b800f888ab338ec0c9c6e0d941161a08ff47aec7
Angelita Vilas Boas <angelitasvb@gmail.com>
2025-07-26 19:01:53 -0300

    alteração do provider

--- terraform/dev/main.tf
+++ terraform/dev/main.tf
@@ -1,6 +1,9 @@
+provider "aws" {
+  region = "sa-east-1"
+}
+
 resource "aws_s3_bucket" "placeholder" {
   bucket = "validador-dev-placeholder"
-  region = "sa-east-1"
 
   lifecycle {
     prevent_destroy = true
