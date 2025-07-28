2b5470328f90e2c9320c6bd9d949dd298cfe0ea3
Angelita Vilas Boas <angelitasvb@gmail.com>
2025-07-28 20:08:30 -0300

    Configuraçao do AuthController

--- src/main/java/com/desafio/validador/controller/PasswordController.java
+++ src/main/java/com/desafio/validador/controller/PasswordController.java
@@ -7,6 +7,7 @@ import com.desafio.validador.service.PasswordService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api")
 public class PasswordController {
