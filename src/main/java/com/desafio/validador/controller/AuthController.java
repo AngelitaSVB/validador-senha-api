bc81ce485ec841644ba9db126db584eb414c3474
Angelita Vilas Boas <angelitasvb@gmail.com>
2025-07-30 18:13:23 -0300

    Atualiza pom

--- src/main/java/com/desafio/validador/controller/AuthController.java
+++ src/main/java/com/desafio/validador/controller/AuthController.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import jakarta.annotation.PostConstruct;
+import javax.annotation.PostConstruct;
 import javax.crypto.SecretKey;
 import java.util.Date;
 import java.util.Map;
@@ -15,55 +15,50 @@ import java.util.Map;
 @RestController
 @RequestMapping("/oauth")
 @CrossOrigin(origins = {
-        "http://localhost:4200",
-<<<<<<< HEAD
-        "https://validador-dev-placeholder.s3-website-sa-east-1.amazonaws.com",
-        "https://87eua3p1ff.execute-api.sa-east-1.amazonaws.com/dev"
-=======
-        "http://validador-dev-placeholder.s3-website-sa-east-1.amazonaws.com",
-        "https://validador-dev-placeholder.s3-website-sa-east-1.amazonaws.com"
->>>>>>> dbe3a63 (Configuração do JWT)
+                "http://localhost:4200",
+                "http://validador-dev-placeholder.s3-website-sa-east-1.amazonaws.com",
+                "https://validador-dev-placeholder.s3-website-sa-east-1.amazonaws.com"
 })
 public class AuthController {
 
-    @Value("${CLIENT_ID}")
-    private String clientId;
+        @Value("${CLIENT_ID}")
+        private String clientId;
 
-    @Value("${CLIENT_SECRET}")
-    private String clientSecret;
+        @Value("${CLIENT_SECRET}")
+        private String clientSecret;
 
-    @Value("${JWT_SECRET}")
-    private String jwtSecret;
+        @Value("${JWT_SECRET}")
+        private String jwtSecret;
 
-    private SecretKey secretKey;
+        private SecretKey secretKey;
 
-    @PostConstruct
-    public void init() {
-        this.secretKey = Keys.hmacShaKeyFor(jwtSecret.getBytes());
-    }
-
-    @PostMapping("/token")
-    public ResponseEntity<?> gerarToken(@RequestParam String grant_type,
-            @RequestParam String client_id,
-            @RequestParam String client_secret) {
-
-        if ("client_credentials".equals(grant_type)
-                && client_id.equals(clientId)
-                && client_secret.equals(clientSecret)) {
-
-            String jwt = Jwts.builder()
-                    .setSubject(client_id)
-                    .setIssuedAt(new Date())
-                    .setExpiration(new Date(System.currentTimeMillis() + 3600000)) // 1 hora
-                    .signWith(secretKey, SignatureAlgorithm.HS256)
-                    .compact();
-
-            return ResponseEntity.ok(Map.of(
-                    "access_token", jwt,
-                    "token_type", "Bearer",
-                    "expires_in", "3600"));
-        } else {
-            return ResponseEntity.status(401).body("Credenciais inválidas");
+        @PostConstruct
+        public void init() {
+                this.secretKey = Keys.hmacShaKeyFor(jwtSecret.getBytes());
         }
-    }
+
+        @PostMapping("/token")
+        public ResponseEntity<?> gerarToken(@RequestParam String grant_type,
+                        @RequestParam String client_id,
+                        @RequestParam String client_secret) {
+
+                if ("client_credentials".equals(grant_type)
+                                && client_id.equals(clientId)
+                                && client_secret.equals(clientSecret)) {
+
+                        String jwt = Jwts.builder()
+                                        .setSubject(client_id)
+                                        .setIssuedAt(new Date())
+                                        .setExpiration(new Date(System.currentTimeMillis() + 3600000)) // 1 hora
+                                        .signWith(secretKey, SignatureAlgorithm.HS256)
+                                        .compact();
+
+                        return ResponseEntity.ok(Map.of(
+                                        "access_token", jwt,
+                                        "token_type", "Bearer",
+                                        "expires_in", "3600"));
+                } else {
+                        return ResponseEntity.status(401).body("Credenciais inválidas");
+                }
+        }
 }
