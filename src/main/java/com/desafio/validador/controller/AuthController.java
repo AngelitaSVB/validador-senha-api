15ec07d28f3f3cd8073ec14f7e4d88011305750b
Angelita <angelitasvb@gmail.com>
2025-07-30 17:31:38 -0300

    atulizado endponts

--- src/main/java/com/desafio/validador/controller/AuthController.java
+++ src/main/java/com/desafio/validador/controller/AuthController.java
@@ -15,7 +15,8 @@ import java.util.Map;
 @RequestMapping("/oauth")
 @CrossOrigin(origins = {
         "http://localhost:4200",
-        "http://validador-dev-placeholder.s3-website-sa-east-1.amazonaws.com"
+        "https://validador-dev-placeholder.s3-website-sa-east-1.amazonaws.com",
+        "https://87eua3p1ff.execute-api.sa-east-1.amazonaws.com/dev"
 })
 public class AuthController {
 
