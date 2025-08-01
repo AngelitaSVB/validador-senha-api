05d70fcda11423025a81c6245a3fbdace401281f
Angelita Vilas Boas <angelitasvb@gmail.com>
2025-08-01 05:12:35 -0300

    Add retorno do token

--- terraform/main.tf
+++ terraform/main.tf
@@ -257,12 +257,11 @@ resource "aws_api_gateway_method" "options_validar" {
 resource "aws_api_gateway_integration" "options_validar_integration" {
   rest_api_id             = aws_api_gateway_rest_api.validador_api.id
   resource_id             = aws_api_gateway_resource.validar.id
-  http_method             = "OPTIONS"
+  http_method             = aws_api_gateway_method.options_validar.http_method
   type                    = "MOCK"
   passthrough_behavior    = "WHEN_NO_MATCH"
-
   request_templates = {
-    "application/json" = "{"statusCode": 200}"
+    "application/json" = "{\"statusCode\": 200}"
   }
 }
 
