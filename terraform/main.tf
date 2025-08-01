bece76c53b8e9f2119aa3295ec4014494e1402d6
Angelita Vilas Boas <angelitasvb@gmail.com>
2025-08-01 04:30:25 -0300

    Atualizacao auth

--- terraform/main.tf
+++ terraform/main.tf
@@ -246,3 +246,69 @@ resource "aws_api_gateway_stage" "validador_stage" {
   rest_api_id   = aws_api_gateway_rest_api.validador_api.id
   stage_name    = "dev"
 }
+
+# ======= BLOCO ADICIONADO: Resposta do método /oauth/token =======
+resource "aws_api_gateway_method_response" "token_response" {
+  rest_api_id = aws_api_gateway_rest_api.validador_api.id
+  resource_id = aws_api_gateway_resource.token.id
+  http_method = aws_api_gateway_method.post_token.http_method
+  status_code = "200"
+
+  response_models = {
+    "application/json" = "Empty"
+  }
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Origin"  = true,
+    "method.response.header.Access-Control-Allow-Headers" = true,
+    "method.response.header.Access-Control-Allow-Methods" = true
+  }
+}
+
+resource "aws_api_gateway_integration_response" "token_integration_response" {
+  rest_api_id = aws_api_gateway_rest_api.validador_api.id
+  resource_id = aws_api_gateway_resource.token.id
+  http_method = aws_api_gateway_method.post_token.http_method
+  status_code = "200"
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Origin"  = "'*'",
+    "method.response.header.Access-Control-Allow-Headers" = "'*'",
+    "method.response.header.Access-Control-Allow-Methods" = "'POST'"
+  }
+
+  depends_on = [aws_api_gateway_integration.token_integration]
+}
+
+# ======= BLOCO ADICIONADO: Resposta do método /api/validar =======
+resource "aws_api_gateway_method_response" "validar_response" {
+  rest_api_id = aws_api_gateway_rest_api.validador_api.id
+  resource_id = aws_api_gateway_resource.validar.id
+  http_method = aws_api_gateway_method.post_validar.http_method
+  status_code = "200"
+
+  response_models = {
+    "application/json" = "Empty"
+  }
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Origin"  = true,
+    "method.response.header.Access-Control-Allow-Headers" = true,
+    "method.response.header.Access-Control-Allow-Methods" = true
+  }
+}
+
+resource "aws_api_gateway_integration_response" "validar_integration_response" {
+  rest_api_id = aws_api_gateway_rest_api.validador_api.id
+  resource_id = aws_api_gateway_resource.validar.id
+  http_method = aws_api_gateway_method.post_validar.http_method
+  status_code = "200"
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Origin"  = "'*'",
+    "method.response.header.Access-Control-Allow-Headers" = "'*'",
+    "method.response.header.Access-Control-Allow-Methods" = "'POST'"
+  }
+
+  depends_on = [aws_api_gateway_integration.validar_integration]
+}
