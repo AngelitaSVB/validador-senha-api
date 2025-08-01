e22f3ad2177036a8f9bcf7668a18c7218b513560
Angelita Vilas Boas <angelitasvb@gmail.com>
2025-08-01 05:00:48 -0300

    Add retorno do token

--- terraform/main.tf
+++ terraform/main.tf
@@ -247,6 +247,55 @@ resource "aws_api_gateway_stage" "validador_stage" {
   stage_name    = "dev"
 }
 
+resource "aws_api_gateway_method" "options_validar" {
+  rest_api_id   = aws_api_gateway_rest_api.validador_api.id
+  resource_id   = aws_api_gateway_resource.validar.id
+  http_method   = "OPTIONS"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "options_validar_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.validador_api.id
+  resource_id             = aws_api_gateway_resource.validar.id
+  http_method             = "OPTIONS"
+  type                    = "MOCK"
+  passthrough_behavior    = "WHEN_NO_MATCH"
+
+  request_templates = {
+    "application/json" = "{"statusCode": 200}"
+  }
+}
+
+resource "aws_api_gateway_method_response" "options_validar_response" {
+  rest_api_id = aws_api_gateway_rest_api.validador_api.id
+  resource_id = aws_api_gateway_resource.validar.id
+  http_method = "OPTIONS"
+  status_code = "200"
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Origin"  = true,
+    "method.response.header.Access-Control-Allow-Methods" = true,
+    "method.response.header.Access-Control-Allow-Headers" = true
+  }
+
+  response_models = {
+    "application/json" = "Empty"
+  }
+}
+
+resource "aws_api_gateway_integration_response" "options_validar_integration_response" {
+  rest_api_id = aws_api_gateway_rest_api.validador_api.id
+  resource_id = aws_api_gateway_resource.validar.id
+  http_method = "OPTIONS"
+  status_code = "200"
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Origin"  = "'*'",
+    "method.response.header.Access-Control-Allow-Methods" = "'POST,OPTIONS'",
+    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,Authorization'"
+  }
+}
+
 # ======= BLOCO ADICIONADO: Resposta do método /oauth/token =======
 resource "aws_api_gateway_method_response" "token_response" {
   rest_api_id = aws_api_gateway_rest_api.validador_api.id
